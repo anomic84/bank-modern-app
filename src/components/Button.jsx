2bc9712ba035a7713652cdc6b7593b7582c56c53
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-12 16:52:25 -0700

    made carddeal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({ styles }) => {
   return (
     <button type="button" className={`py-4 px-6 bg-blue-gradient
-    font-popping font-medium text-[18px] text-primary outline-none`}>
+    font-popping font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
       Get Started
     </button>
   )
