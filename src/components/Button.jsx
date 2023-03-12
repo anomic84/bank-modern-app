480036281dbfb968e925e6769b10750a125a3814
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-12 16:23:01 -0700

    finished business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient
+    font-popping font-medium text-[18px] text-primary outline-none`}>
+      Get Started
+    </button>
   )
 }
 
